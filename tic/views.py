12c06ddff423f02373c46f1d5d6b0d65f8c69c97
Reverend Scandal <a@debian>
2014-03-29 13:17:16 -0400

    Signifigant changes to view functions in regards to calculating the automanted players nnext move.

--- tic/views.py
+++ tic/views.py
@@ -1,99 +1,77 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
-
 from tic.models import Board
 from tic.forms import BoardForm
 
-				
-wingroups=[	["zero","one","two"], 
-		["three","four","five"], 
-		["six","seven","eight"],
-		["zero","three","six"],
-		["one","four","seven"],
-		["two","five","eight"], 
-		["zero","four","eight"], 
-		["two","four","six"]  
+# All of this is about to be a tablem, this is getting silly.		
+wingroups=[	 {"zero": '', "four": '', "eight": ''}, {"two": '',"four": ' ',"six": ''},
+		  {"three": ' ',"four": ' ',"five":''}, {"one": ' ',"four": ' ',"seven": ''},
+		  {"zero": ' ',"one":'',"two":''},	{"six": ' ',"seven": ' ',"eight": ''},
+		  {"zero": ' ',"three":'',"six": ' '},{"two": ' ',"five": ' ',"eight": ''}
 	]
-	
-	
-midgroups=[	["zero","one","three"],
-		["one","two","five"],
-		["three","six","seven"],
-		["seven","eight","five"]
+
+midgroups=[	["zero","one","three"],	["seven","eight","five"]
+		["one","two","five"],["three","six","seven"],
 	]		
+
+xmoves=["four","zero","one","three","five","two","seven","six","eight"]
+
+def updateWinGroups(pv):
 	
-xmoves=["four","one","zero","three","five","two","seven","six","eight"]
-xolist=[u'x',u'o']
 
 
-def convGroup(group,pv,xolist):
-	xout=[]
-	free=[]
-	for g in group:
-		c=pv[g].strip()
-		if c in xolist:
-			xout.append(c)
-		else:
-			free.append(g)
+def mkPvg(pv,grp):
+	pvd={}
+	for g in grp:
+		 
+		pvd[g]=(pv[g])
+	#print 'pvd', pvd
+		
+	return pvd
 
-	return xout,free			
-
-
-
-def twoOfThree(group,pv):
-	print group
-	pick='no'
-	xo='no'	
-	xolist=[u'x',u'o']
-	xout,free=convGroup(group,pv,xolist)
-	print xout
-	print free, 'free'
-	xout=list(set(xout))
-	if len(xout)==1 and len(free)==1:
-		return xout[0], free[0]	
-	
-	
-	return xo,pick
-				
-def chkGroups(groups,pv):
-	xo='no'
-	pick='no'
-	for group in groups:
-		xo,pick= twoOfThree(group,pv)
-		print pick, 'no pick'
-		if xo != 'no':
-			print 'chkGroups'
-			print xo, pick
+def chkGrps(pv,grps,xo):
+	for grp in grps:
+		pvd=mkPvg(pv,grp)
+		#pvg=mkPvg(pv,grp)
+		
+		pval=pvd.values()
+		pk=pvd.keys(
+		if pval.count(xo)==2 and pval.count(u'')==1:
+		
+			pick =pk[pval.index('')]
+			print 'new pick', pick 
+			return pick,pk
 			
-			return xo,pick
-			
-	return xo,pick		
-	
+
 
 def findOpen(xmoves,pv):
-	for move in xmoves:
-		print pv[move]
-		
-		if pv[move] in xolist:
-			xmoves.remove(move)
-		else:	
-			print move
-			return move
-		
-		
 	
-def mkMove(pv):
-	allgroups=wingroups+midgroups
-	xo,pick=chkGroups(wingroups,pv)
-	
-	if xo !='no':
-		return pick
-	else:
-		pick=findOpen(xmoves,pv)
-		return pick	
-	
+	for xm in xmoves:
+		if xm in pv.values():
+			return xm
 
+	return 'k'
 
+def mkPick(pv):
+	p='k'
+	winners=['k'] 
+
+	
+	
+	pick,winners=chkGrps(pv,wingroups,'x')
+		print 'x', pick
+		print 'wingroup', winners
+	
+	except:
+		try:	
+			pick,devnull= chkGrps(pv,wingroups,'o')
+			print 'o' ,pick
+
+		except:
+			p=findOpen(xmoves,pv)
+			
+	return p,winners
+	
 
 
 class BoardCreate(CreateView):
@@ -101,29 +79,23 @@ class BoardCreate(CreateView):
 	fields = '__all__'
 
 def play(request):
-   	print request.method 
+   	
     	if request.method == 'GET':
-
-	
 		form=BoardForm()
-	
+		freshboard=1
+		return render(request,'tic/board_form.html',{'form':form,'freshboard':1})	
 
     	if request.method == 'POST':
 		post_values=request.POST.copy()
-		pick=mkMove(post_values)
-		print 'pick returned', pick		  
+		form = BoardForm(post_values)
 		
-		post_values[pick]=u'x'
-		
-    		form = BoardForm(post_values)
-		
-		
-		
+		pick,winners=mkPick(post_values)
+		print 'pick--->', pick
+		print 'winners --->',winners
 		if form.is_valid():
-            		board = form.save()
-            		board.save()
-	
-	return render(request,'tic/board_form.html',{'form':form})	
+            	
+			return render(request,'tic/board_form.html',{'form':form,'pick':pick,'wingroup':winners})	
 
-
+		
+		return render(request,'tic/board_form.html',{'form':form,'pick':pick,'wingroup':winners})	
 	
